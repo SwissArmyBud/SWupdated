6fb7ec1318c70a672e356e9c150db263dba9801c
Andrew Porter <swissarmybud@gmail.com>
2017-09-18 21:50:55 -0700

    Rename REMOVE.KEY to capitalized version

--- tools/makeupdate.sh
+++ tools/makeupdate.sh
@@ -1,5 +1,5 @@
 CONTAINER_VER="1.0"
 PRODUCT_NAME="my-swupdate"
-FILES="sw-description remove.key"
+FILES="sw-description REMOVE.KEY"
 for i in $FILES;do
         echo $i;done | cpio -ov -H crc >  update.swu
